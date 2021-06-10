f7a49e7956aa586c0fa519aef5159344b11072cc
Tenzin <tznklsng28@gmail.com>
2021-06-09 20:02:09 -0400

    some changes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 function medianScore(students) {
   const sortList = students.sort((a, b) => a.score - b.score);
-  const midpoint = Math.floor(sortpList.length / 2);
+  const midpoint = Math.floor(sortList.length / 2);
 
   return sortList[midpoint].score;
 }
